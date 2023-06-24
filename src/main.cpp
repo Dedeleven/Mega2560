cac03b9390e10d8d6d2588809eb794a251cbd07f
JulienDedelley <julien.dedelley@gmail.com>
2023-06-24 15:57:36 +0200

    One RGB led doing a rainbow

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,60 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+// Define Pins
+#define BLUE 3
+#define GREEN 5
+#define RED 6
+
+void setup()
+{
+  pinMode(RED, OUTPUT);
+  pinMode(GREEN, OUTPUT);
+  pinMode(BLUE, OUTPUT);
+  // digitalWrite(RED, LOW);
+  // digitalWrite(GREEN, LOW);
+  // digitalWrite(BLUE, LOW);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
+// declare variable
+int redValue;
+int greenValue;
+int blueValue;
+int delayTime;
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void loop()
+{
+  // define variables
+  redValue = 255;
+  greenValue = 0;
+  blueValue = 0;
+  delayTime = 10;
+
+  for (int i = 0; i <255; i++ )
+  {
+    redValue -= 1;
+    greenValue += 1;
+    analogWrite(RED, redValue);
+    analogWrite(GREEN, greenValue);
+    delay(delayTime);
+  }
+
+  for (int i = 0; i <255; i++ )
+  {
+    greenValue -= 1;
+    blueValue += 1;
+    analogWrite(GREEN, greenValue);
+    analogWrite(BLUE, blueValue);
+    delay(delayTime);
+  }
+
+  for (int i = 0; i <255; i++ )
+  {
+    blueValue -= 1;
+    redValue += 1;
+    analogWrite(BLUE, blueValue);
+    analogWrite(RED, redValue);
+    delay(delayTime);
+  }
+
 }
